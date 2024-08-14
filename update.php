1ad848ab34634b77907d02b79875b546f54531e9
padil <113089260+padilpplg2@users.noreply.github.com>
2024-08-14 11:20:38 +0700

    update.php

--- update.php
+++ update.php
@@ -1,9 +1,10 @@
 <?php
 include 'koneksi.php';
+$id = $_POST['id'];
 $nama = $_POST['nama'];
 $umur = $_POST['umur'];
 
-$sql = "UPDATE siswa set umur='$umur' where nama='$nama'";
+$sql = "UPDATE siswa set nama='$nama',umur='$umur' where id='$id'";
 $query = mysqli_query($koneksi, $sql);
 if (!$query) {
 die(mysqli_error($koneksi));
