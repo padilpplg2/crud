4af930d70dd8c59a3c5e07b6e72bfb734248812f
padil <113089260+padilpplg2@users.noreply.github.com>
2024-08-14 11:19:40 +0700

    proses.php

--- proses.php
+++ proses.php
@@ -1,10 +1,9 @@
 <?php
 include 'koneksi.php';
-
 $nama = $_POST['nama'];
 $umur = $_POST['umur'];
 
-$sql = "INSERT INTO siswa VALUES('$nama','$umur')";
+$sql = "INSERT INTO siswa VALUES('','$nama','$umur')";
 $query = mysqli_query($koneksi, $sql);
 header("Location: index.php");
 ?>
